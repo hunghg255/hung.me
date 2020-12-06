b29c6019727ded4592767c9de498797c85b7f6a3
Silver Miu <giahunghust@gmail.com>
2020-12-06 17:17:26 +0700

    fix

--- src/components/SEO/Seo.tsx
+++ src/components/SEO/Seo.tsx
@@ -10,23 +10,27 @@ export default function Seo() {
       <Helmet>
         <meta charSet='utf-8' />
         <title>Hunghg | Front-end Developer</title>
+
+        <meta property='og:type' content='website' />
+
         <link rel='icon' href={ImgFavicon} />
 
-        <meta name='title' content='Front-end Developer' />
+        <meta name='title' content='Hunghg | Front-end Developer' />
         <meta name='description' content="I'm Hung an Front-end Developer" />
 
         <meta property='og:type' content='website' />
-        <meta
-          property='og:url'
-          content='https://hunghg255.github.io/portfolio'
-        />
-
-        <meta property='og:title' content='Front-end Developer' />
-        <meta
-          property='og:description'
-          content="I'm Hung an Front-end Developer"
-        />
+        <meta property='og:url' content='https://portfolio-c0129.web.app/' />
+        <meta property='og:title' content='Hunghg | Front-end Developer' />
+        <meta property='og:description' content="I'm Hung an Front-end Developer" />
         <meta property='og:image' content={ImgSeo} />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://portfolio-c0129.web.app/' />
+        <meta property='twitter:title' content='Hunghg | Front-end Developer' />
+        <meta property='twitter:description' content="I'm Hung an Front-end Developer" />
+        <meta property='twitter:image' content={ImgSeo} />
+
+        <meta property='og:image:type' content='image/jpeg' />
       </Helmet>
     </div>
   );
