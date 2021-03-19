3cb19b4a012091baae1e708e0ccbc12c911eee38
Silver Miu <giahunghust@gmail.com>
2021-03-20 00:27:41 +0700

    update

--- src/components/SEO/Seo.tsx
+++ src/components/SEO/Seo.tsx
@@ -10,7 +10,7 @@ export default function Seo() {
 
         <meta property='og:type' content='website' />
 
-        <link rel='icon' href={'https://raw.githubusercontent.com/hunghg255/my-portfolio/main/src/Image/favicon.png'} />
+        <link rel='icon' href='https://raw.githubusercontent.com/hunghg255/my-portfolio/main/src/Image/favicon.png' />
 
         <meta name='title' content='Hunghg | Front-end Developer' />
         <meta name='description' content="I'm Hung an Front-end Developer. I got a bachelor of Electronics Telecommunication Engineering at Ha Noi University of Science and Technology (2015 - 2020)" />
@@ -19,13 +19,13 @@ export default function Seo() {
         <meta property='og:url' content='https://portfolio-c0129.web.app/' />
         <meta property='og:title' content='Hunghg | Front-end Developer' />
         <meta property='og:description' content="I'm Hung an Front-end Developer. I got a bachelor of Electronics Telecommunication Engineering at Ha Noi University of Science and Technology (2015 - 2020)" />
-        <meta property='og:image' content={'https://raw.githubusercontent.com/hunghg255/my-portfolio/main/src/Image/me.JPG'} />
+        <meta property='og:image' content='https://raw.githubusercontent.com/hunghg255/my-portfolio/main/src/Image/me.JPG' />
 
         <meta property='twitter:card' content='summary_large_image' />
         <meta property='twitter:url' content='https://portfolio-c0129.web.app/' />
         <meta property='twitter:title' content='Hunghg | Front-end Developer' />
         <meta property='twitter:description' content="I'm Hung an Front-end Developer. I got a bachelor of Electronics Telecommunication Engineering at Ha Noi University of Science and Technology (2015 - 2020)" />
-        <meta property='twitter:image' content={'https://raw.githubusercontent.com/hunghg255/my-portfolio/main/src/Image/me.JPG'} />
+        <meta property='twitter:image' content='https://raw.githubusercontent.com/hunghg255/my-portfolio/main/src/Image/me.JPG' />
 
         <meta property='og:image:type' content='image/jpeg' />
       </Helmet>
