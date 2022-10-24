a8f830959077779d73e86731000984655faee33d
hunghg255 <hunghg.agiletech@gmail.com>
2022-10-24 18:43:59 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
+import Lenis from '@studio-freight/lenis';
 import Head from 'next/head';
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: 'vertical', // vertical, horizontal
+      gestureDirection: 'vertical', // vertical, horizontal, both
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      window.cancelAnimationFrame(raf);
+    };
+  }, []);
+
   return (
     <>
       <Head>
