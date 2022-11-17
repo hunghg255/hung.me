ae8e0f0b94ce3a0818f8ea8933d23adf2c684fa8
hunghg255 <hunghg.agiletech@gmail.com>
2022-11-17 22:14:27 +0700

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -9,6 +10,28 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap'
             rel='stylesheet'
           />
+          <Script
+            id='gtag-base'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KGLG29S');
+          `,
+            }}
+          />
+
+          <noscript>
+            <iframe
+              src='https://www.googletagmanager.com/ns.html?id=GTM-KGLG29S'
+              height='0'
+              width='0'
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
         </Head>
         <body>
           <Main />
