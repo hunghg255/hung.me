43f1f2558d5d17e860a4e4d6a5a78d76dcbd9f4d
hunghg255 <hunghg.agiletech@gmail.com>
2022-11-17 22:45:35 +0700

    update ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,14 +36,14 @@ class MyDocument extends Document {
           <Script
             strategy='lazyOnload'
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=GTM-KGLG29S`}
+            src={`https://www.googletagmanager.com/gtag/js?id=G-WZW3J8S7EF`}
           ></Script>
           <Script id='google-analytics' strategy='lazyOnload'>
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'GTM-KGLG29S');
+              gtag('config', 'G-WZW3J8S7EF');
             `}
           </Script>
         </Head>
