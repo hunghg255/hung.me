d0ec4f7ea72373d2fe092efce094e117e2fa50f4
hunghg255 <hunghg.agiletech@gmail.com>
2022-12-13 16:15:47 +0700

    add loading

--- pages/blurhash.tsx
+++ pages/blurhash.tsx
@@ -19,9 +19,12 @@ const BlurHash = () => {
   const refInput: any = useRef();
   const [blurhash, setBlurhash] = useState<any>('');
   const [currentImage, setCurrentImage] = useState<any>();
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = async () => {
+    if (loading) return;
     try {
+      setLoading(true);
       let formData = new FormData();
       formData.append('file', refInput.current?.files?.[0]);
       const base64 = await toBase64(refInput.current?.files?.[0]);
@@ -30,11 +33,11 @@ const BlurHash = () => {
         method: 'post',
         body: formData,
       }).then((r) => r.json());
+      setLoading(false);
       setBlurhash(r);
       setCurrentImage(base64);
     } catch (error) {}
   };
-  console.log(blurhash);
 
   return (
     <>
@@ -42,7 +45,7 @@ const BlurHash = () => {
       <br />
 
       <input type={'file'} ref={refInput} />
-      <button onClick={onSubmit}>Submit</button>
+      <button onClick={onSubmit}>{loading ? 'Loading...' : 'Submit'}</button>
       <br />
 
       <div>
@@ -53,7 +56,6 @@ const BlurHash = () => {
               <textarea value={blurhash?.blurhash?.base64} />
             )}
             <br />
-            <code>asdasd</code>
 
             <br />
 
