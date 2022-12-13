37b72aacb3f40ad7aa03d6dc0b04dcfe0ac9a5e9
hunghg255 <hunghg.agiletech@gmail.com>
2022-12-13 16:10:46 +0700

    update blurhash

--- pages/blurhash.tsx
+++ pages/blurhash.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 import React, { useRef, useState } from 'react';
 
 // const QuillC = dynamic(() => import('src/components/Quill/Quill'), {
@@ -16,6 +18,7 @@ export const toBase64 = (file: any) =>
 const BlurHash = () => {
   const refInput: any = useRef();
   const [blurhash, setBlurhash] = useState<any>('');
+  const [currentImage, setCurrentImage] = useState<any>();
 
   const onSubmit = async () => {
     try {
@@ -23,27 +26,73 @@ const BlurHash = () => {
       formData.append('file', refInput.current?.files?.[0]);
       const base64 = await toBase64(refInput.current?.files?.[0]);
 
-      const r = await fetch('http://localhost:3000/api/blurhash', {
-        body: JSON.stringify(base64),
+      const r = await fetch('https://blurhash-image.onrender.com/blurhash', {
         method: 'post',
+        body: formData,
       }).then((r) => r.json());
       setBlurhash(r);
+      setCurrentImage(base64);
     } catch (error) {}
   };
+  console.log(blurhash);
 
   return (
     <>
       <h1>Get BlurHash Image</h1>
+      <br />
+
       <input type={'file'} ref={refInput} />
       <button onClick={onSubmit}>Submit</button>
+      <br />
 
       <div>
+        <br />
         {blurhash?.blurhash?.base64 && (
-          <textarea value={blurhash?.blurhash?.base64} />
+          <>
+            {blurhash?.blurhash?.base64 && (
+              <textarea value={blurhash?.blurhash?.base64} />
+            )}
+            <br />
+            <code>asdasd</code>
+
+            <br />
+
+            <div>
+              <h2>
+                <strong>Current Image</strong>
+              </h2>
+
+              <img
+                src={currentImage}
+                alt=''
+                style={{
+                  width: 300,
+                  height:
+                    300 /
+                    (blurhash.blurhash.img.width /
+                      blurhash.blurhash.img.height),
+                }}
+              />
+              <br />
+              <h2>
+                <strong>Blurhash Image</strong>
+              </h2>
+
+              <img
+                src={blurhash?.blurhash?.base64}
+                alt=''
+                style={{
+                  width: 300,
+                  height:
+                    300 /
+                    (blurhash.blurhash.img.width /
+                      blurhash.blurhash.img.height),
+                }}
+              />
+            </div>
+          </>
         )}
       </div>
-
-      {/* <QuillC /> */}
     </>
   );
 };
