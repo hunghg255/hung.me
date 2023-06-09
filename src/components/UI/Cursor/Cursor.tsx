3f1fbbd090b18179170fa2ca533ff42a2941f378
hunghg255 <giahunghust@gmail.com>
2023-06-09 17:49:12 +0700

    update

--- src/components/UI/Cursor/Cursor.tsx
+++ src/components/UI/Cursor/Cursor.tsx
@@ -2,6 +2,40 @@ import { FC, useEffect, useRef } from 'react';
 
 import styles from './index.module.css';
 
+function getAngle(diffX: number, diffY: number) {
+  return (Math.atan2(diffY, diffX) * 180) / Math.PI;
+}
+
+function getSqueeze(diffX: number, diffY: number) {
+  const distance = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+  const maxSqueeze = 0.15;
+  const accelerator = 750;
+  return Math.min(distance / accelerator, maxSqueeze);
+}
+
+export function useCursor() {
+  const pos = { x: 0, y: 0 }; // cursor's coordinates
+  const speed = 0.1; // between 0 and 1
+
+  const updateCursor = ({ x, y }) => {
+    const diffX = Math.round(x - pos.x);
+    const diffY = Math.round(y - pos.y);
+
+    pos.x += diffX * speed;
+    pos.y += diffY * speed;
+
+    const angle = getAngle(diffX, diffY);
+    const squeeze = getSqueeze(diffX, diffY);
+
+    const scale = `scale(${1 + squeeze}, ${1 - squeeze})`;
+    const rotate = `rotate(${angle}deg)`;
+
+    return { transform: rotate + scale };
+  };
+
+  return { updateCursor };
+}
+
 const Cursor: FC = () => {
   const isFirstMove = useRef(true);
 
@@ -15,6 +49,8 @@ const Cursor: FC = () => {
     x: 0,
     y: 0,
   });
+
+  const { updateCursor } = useCursor();
 
   useEffect(() => {
     if (window.matchMedia('(pointer: coarse)').matches) return;
@@ -36,14 +72,15 @@ const Cursor: FC = () => {
     const updateMouse = () => {
       requestAnimationFrame(updateMouse);
 
-      displayedMouse.current.x +=
-        (realMouse.current.x - displayedMouse.current.x) * 0.1;
-      displayedMouse.current.y +=
-        (realMouse.current.y - displayedMouse.current.y) * 0.1;
+      displayedMouse.current.x += (realMouse.current.x - displayedMouse.current.x) * 0.1;
+      displayedMouse.current.y += (realMouse.current.y - displayedMouse.current.y) * 0.1;
+
+      const styles = updateCursor({ x: realMouse.current.x, y: displayedMouse.current.y });
 
       if (cursorRef.current) {
         cursorRef.current.style.left = `${displayedMouse.current.x}px`;
         cursorRef.current.style.top = `${displayedMouse.current.y}px`;
+        cursorRef.current.style.transform = `translate(-50%, -50%) ${styles.transform}`;
       }
     };
 
