629bd96eef683548bfa8ad7623c3f33eb1d35c56
hunghg255 <giahunghust@gmail.com>
2023-07-26 00:56:38 +0700

    revert

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-
 import dynamic from 'next/dynamic';
 
 import Header from 'src/components/Section/Header';
@@ -10,14 +8,6 @@ const Footer = dynamic(() => import('src/components/Section/Footer'));
 const Portfolio = dynamic(() => import('src/components/Section/Portfolio'));
 
 function Home() {
-  useEffect(() => {
-    fetch(
-      'https://foundation.miraeasset.com.vn/api/header?populate=deep&locale=en&fbclid=IwAR3jTHEl0En6LiLzJefH4LLTh4sndrIY-hvvg3kFUXWqhL09kvjHJuxtpiY',
-    )
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-  }, []);
-
   return (
     <>
       <Header />
