e431d452f37bb5a30eadd174ecf431ba66489686
hunghg255 <giahunghust@gmail.com>
2023-07-26 00:48:39 +0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import dynamic from 'next/dynamic';
 
 import Header from 'src/components/Section/Header';
@@ -8,6 +10,14 @@ const Footer = dynamic(() => import('src/components/Section/Footer'));
 const Portfolio = dynamic(() => import('src/components/Section/Portfolio'));
 
 function Home() {
+  useEffect(() => {
+    fetch(
+      'https://foundation.miraeasset.com.vn/api/header?populate=deep&locale=en&fbclid=IwAR3jTHEl0En6LiLzJefH4LLTh4sndrIY-hvvg3kFUXWqhL09kvjHJuxtpiY',
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <>
       <Header />
