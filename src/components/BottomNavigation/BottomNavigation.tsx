11d2b0e0a216c0c44a03ebd4ee7b9d03749000b0
hunghg255 <giahunghust@gmail.com>
2024-09-28 13:06:04 +0700

    chore: update

--- src/components/BottomNavigation/BottomNavigation.tsx
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -33,67 +33,86 @@ const ButtonToggleTheme = () => {
 };
 
 const BottomNavigation = () => {
+  useEffect(() => {
+    if (document) {
+      const nav: any = document.querySelector('.nav'),
+        nav_inner = nav.querySelector('.nav-inner');
+
+      nav_inner.querySelectorAll('li').forEach((li, idx) => {
+        li.addEventListener('mouseleave', () => nav.style.setProperty('--enter-nav', 0));
+        li.addEventListener('mousemove', (e) => {
+          let { clientX: x, clientY: y } = e;
+          const rect = li.getBoundingClientRect();
+          x = (x - rect.x - rect.width / 2) / rect.width;
+          y = (y - rect.y - rect.height / 2) / rect.height;
+
+          nav.style = `
+            --enter-nav: 1;
+            --current-item: ${idx};
+            --x: ${x};
+            --y: ${y};
+        `;
+        });
+      });
+    }
+  }, []);
+
   return (
-    <motion.div
-      className={styles.containerBottomNavigation}
-      initial={{ opacity: 0, bottom: 0 }}
-      animate={{ opacity: 1, bottom: 33 }}
-      transition={{
-        duration: 0.8,
-        delay: 0.8,
-        ease: [0, 0.71, 0.2, 1.01],
-      }}
-    >
-      <ul>
-        <li className={styles.navItem}>
-          <ActiveLink href='/' aria-label='Home' activeClassName={styles.active}>
-            <Icon icon='icon-home' />
-          </ActiveLink>
-          <span>Home</span>
-        </li>
-        <li className={styles.navItem}>
-          <ActiveLink href='/projects' aria-label='Projects' activeClassName={styles.active}>
-            <Icon icon='icon-pointer' />
-          </ActiveLink>
-          <span>Projects</span>
-        </li>
+    <div className={styles.containerBottomNavigation}>
+      <nav className='nav'>
+        <ul className='binoculars'>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Blogs</li>
+          <li>Github</li>
+          <li>X</li>
+          <li>Theme</li>
+        </ul>
+        <ul className='nav-inner'>
+          <li className={styles.navItem}>
+            <ActiveLink href='/' aria-label='Home' activeClassName={styles.active}>
+              <Icon icon='icon-home' />
+            </ActiveLink>
+          </li>
+          <li className={styles.navItem}>
+            <ActiveLink href='/projects' aria-label='Projects' activeClassName={styles.active}>
+              <Icon icon='icon-pointer' />
+            </ActiveLink>
+          </li>
 
-        <li className={styles.navItem}>
-          <ActiveLink href='/blogs' aria-label='Blog' activeClassName={styles.active}>
-            <Icon icon='icon-glasses' />
-          </ActiveLink>
-          <span>Blog</span>
-        </li>
+          <li className={styles.navItem}>
+            <ActiveLink href='/blogs' aria-label='Blog' activeClassName={styles.active}>
+              <Icon icon='icon-glasses' />
+            </ActiveLink>
+          </li>
 
-        <li className={styles.navItem}>
-          <a
-            href='https://github.com/hunghg255'
-            target='_blank'
-            rel='noreferrer'
-            aria-label='Github'
-          >
-            <Icon icon='icon-github' />
-          </a>
-          <span>Github</span>
-        </li>
-        <li className={styles.navItem}>
-          <a
-            href='https://twitter.com/hunghg255'
-            target='_blank'
-            rel='noreferrer'
-            aria-label='Twitter'
-          >
-            <Icon icon='icon-x' />
-          </a>
-          <span>X</span>
-        </li>
+          <li className={styles.navItem}>
+            <a
+              href='https://github.com/hunghg255'
+              target='_blank'
+              rel='noreferrer'
+              aria-label='Github'
+            >
+              <Icon icon='icon-github' />
+            </a>
+          </li>
+          <li className={styles.navItem}>
+            <a
+              href='https://twitter.com/hunghg255'
+              target='_blank'
+              rel='noreferrer'
+              aria-label='Twitter'
+            >
+              <Icon icon='icon-x' />
+            </a>
+          </li>
 
-        <li className={styles.navItem}>
-          <ButtonToggleTheme />
-          <span>Toggle Theme</span>
-        </li>
-      </ul>
-    </motion.div>
+          <li className={styles.navItem}>
+            <ButtonToggleTheme />
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
