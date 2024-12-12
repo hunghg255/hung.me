160ca2f6dea1f2bd59f2b27504868816c57d9ea3
hunghg255 <giahunghust@gmail.com>
2024-12-12 10:13:39 +0700

    chore: update

--- .contentlayer/generated/index.mjs
+++ .contentlayer/generated/index.mjs
@@ -4,7 +4,7 @@ export { isType } from 'contentlayer2/client'
 
 // NOTE During development Contentlayer imports from `.mjs` files to improve HMR speeds.
 // During (production) builds Contentlayer it imports from `.json` files to improve build performance.
-import { allWorks } from './Works/_index.mjs'
+import allWorks from './Works/_index.json' with { type: 'json' }
 
 export { allWorks }
 
